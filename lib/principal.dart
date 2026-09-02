347c2c19d4de67e5104f6d6dd21ca89748e8de65
nallely-lopez <lopezgarcializethnallely@gmail.com>
2026-09-01 21:43:12 -0600

    Creación del body en el archivo principal.dart

--- lib/principal.dart
+++ lib/principal.dart
@@ -15,8 +15,23 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> { //Clase que def
       // Aquí iremos agregando las piezas del Scaffold
       appBar: AppBar( // AppBar es un widget que proporciona una barra de navegación en la parte superior de la pantalla
         title: const Text('Anatomía del Scaffold'), // Título de la barra de navegación
-        backgroundColor: const Color.fromARGB(255, 85, 150, 150), // Color de fondo de la barra de navegación
-        foregroundColor: const Color.fromARGB(255, 2, 3, 39), // Color del texto de la barra de navegación
+        backgroundColor: Colors.teal, // Color de fondo de la barra de navegación
+        foregroundColor: const Color.fromARGB(255, 255, 255, 255), // Color del texto de la barra de navegación
+      ),
+
+      body: const Center( // Center es un widget que centra su hijo en la pantalla
+        child: Column( // Column es un widget que organiza a sus hijos en una columna vertical
+          mainAxisAlignment: MainAxisAlignment.center, // mainAxisAlignment es una propiedad que define cómo se alinean los hijos en el eje principal de la columna, en este caso, se alinean al centro
+          children: [ // children es una propiedad que define los hijos de la columna, en este caso, son un icono y dos textos
+            Icon(Icons.widgets, size: 80, color: Colors.teal), // Icon es un widget que muestra un icono, en este caso, el icono de widgets, con un tamaño de 80 y un color de teal
+            SizedBox(height: 20), // SizedBox es un widget que crea un espacio vacío con un tamaño específico, en este caso, un espacio de 20 de alto
+            Text( // Text es un widget que muestra un texto, en este caso, el texto
+              'Este es el BODY',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold), // style es una propiedad que define el estilo del texto, en este caso, un tamaño de 24 y un peso de negrita
+            ),
+            Text('Aqui va el contenido principal de la pantalla.'), // Text es un widget que muestra un texto, en este caso, el texto
+          ],
+        ),
       ),
     );
   }
