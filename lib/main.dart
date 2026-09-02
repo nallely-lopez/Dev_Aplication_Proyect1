1a9e365c6fbbdd60648f757e0d32c1a3bce5438c
nallely-lopez <lopezgarcializethnallely@gmail.com>
2026-09-01 21:16:50 -0600

    Actualización de código en el archivo principal.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: PantallaPrincipal(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
